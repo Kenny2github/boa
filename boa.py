4227d703f9b285ec2ca45e530a9f53f1f72a30d5
Kenny2github <kenny2minecraft@gmail.com>
2018-10-25 19:18:28 +0800

    A lot of rewriting

--- boa.py
+++ boa.py
@@ -1,14 +1,20 @@
 import sys
 import re
 import builtins
+import argparse
+import io
+import functools
+import operator
 
 stack = []
 variables = {}
 lineno = 0
 line = ''
+defining = None
 
 def _error(exc):
-    raise exc
+    print(type(exc).__name__, str(exc) + ' (line {} {})'.format(lineno, line), sep=': ', file=sys.stderr)
+    sys.exit(1)
 
 def _empty():
     _error(RuntimeError('stack is empty'))
@@ -29,14 +35,37 @@ def printstack():
 
 builtins.printstack = printstack
 
-with open(sys.argv[1]) as f:
+parser = argparse.ArgumentParser(description='Run a Boa file.')
+parser.add_argument('file', help='the Boa file to read from')
+parser.add_argument('-C', action='store_true', help='if specified, do not parse comments in strings (i.e. treat the # and everything after as part of the string)')
+cmdargs = parser.parse_args()
+
+def run_boa(fileobj, call=lineno, ret=None):
+    global lineno, line, stack, variables, defining
+    lineno = call
     while 1:
-        lineno += 1
         try:
             line = f.readline().rstrip('\n')
         except EOFError:
             break
-        if re.search('^[0-9]', line): #number
+        if not line:
+            break
+        if defining and not line.startswith('>'):
+            defining[2].write(line + '\n')
+            continue
+        elif defining: #i.e. the line starts with >
+            newfunc = functools.partial(
+                run_boa, defining[2], defining[1], lineno
+            )
+            variables[defining[0]] = newfunc
+            defining = None
+            continue
+        if not (cmdargs.C and line.startswith(('"', "'"))):
+            line = line.partition('#')[0] #get rid of comments
+        if line.startswith('<'): #start function definition
+            name = line[1:]
+            defining = (name, lineno + 1, io.StringIO())
+        elif re.search('^[0-9]', line): #number
             try:
                 num = int(line, 0)
             except ValueError:
@@ -62,8 +91,14 @@ with open(sys.argv[1]) as f:
             stack.append(o(*args))
         elif hasattr(builtins, line): #builtin
             stack.append(getattr(builtins, line))
+        elif hasattr(operator, line): #operator
+            stack.append(getattr(operator, line))
         else: #variable
             if line not in variables: #pop from stack and set variable if name not known
                 variables[line] = _pop()
             else: #append variable to stack if name known
                 stack.append(variables[line])
+        lineno += 1
+
+with open(cmdargs.file) as f:
+    run_boa(f)
